ed36f3ee68cd7b09c7100e0d365292e5af92e6e6
Yemmm <>
2021-05-27 07:47:44 +0000

    Completed Tutorial

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 const Discord = require("discord.js")
 const fetch = require("node-fetch")
+const keepAlive = require("./server")
+const Database = require("@replit/database")
+
+const db = new Database()
 const client = new Discord.Client()
+
 const mySecret = process.env['TOKEN']
 
 const sadWords = ["sad", "depressed", "unhappy", "angry"]
 
-const encouragements = [
+const starterEncouragements = [
   "Cheer up!",
   "Hang in there.",
   "You are a great person / bot"
 ]
+
+db.get("encouragements").then(encouragements => {
+  if (!encouragements || starterEncouragements.length < 1){
+    db.set("encouragements", starterEncouragements )
+  }
+})
+
+db.get("responding").then(value => {
+  if (value == null){
+    db.set("responding", true)
+  }
+})
+
+function updateEncouragements(encouragingMessage){
+  db.get("encouragements").then(encouragements => {
+    encouragements.push([encouragingMessage])
+    db.set("encouragements", encouragements)
+  })
+}
+
+function deleteEncouragement(index){
+  db.get("encouragements").then(encouragements => {
+    if(encouragements.length > index){
+      encouragements.splice(index, 1)
+      db.set("encouragements", encouragements)
+    }
+  })
+}
 
 function getQuote(){
   return fetch("https://zenquotes.io/api/random")
@@ -32,10 +65,44 @@ client.on("message", msg => {
     getQuote().then(quote => msg.channel.send(quote))
   }
 
-  if (sadWords.some(word => msg.content.includes(word))) {
-  const encouragement = encouragements[Math.floor(Math.random() * encouragements.length)]
-  msg.reply(encouragement)
+  db.get("responding").then(responding =>{
+      if (responding && sadWords.some(word => msg.content.includes(word))) {
+        db.get("encouragements").then(encouragements => {
+        const encouragement = encouragements[Math.floor(Math.random() * encouragements.length)]
+        msg.reply(encouragement)
+      })
+    }
+  })
+
+  if (msg.content.startsWith("$new")){
+    encouragingMessage = msg.content.split("$new ") [1]
+    updateEncouragements(encouragingMessage)
+    msg.channel.send("New encouraging message added.")
+  }
+
+  if (msg.content.startsWith("$del")){
+    index = parseInt(msg.content.split("$del ") [1])
+    deleteEncouragement(index)
+    msg.channel.send("Encouraging message deleted.")
+  }
+
+  if(msg.content.startsWith("$list")){
+    db.get("encouragements").then(encouragements => {
+      msg.channel.send(encouragements)
+    })
+  }
+
+  if(msg.content.startsWith("$responding")){
+    value = msg.content.split("$responding ")[1]
+      if(value.toLowerCase() == "true"){
+        db.set("responding", true)
+        msg.channel.send("Responding is on.")
+      } else {
+        db.set("responding", false)
+        msg.channel.send("Responding is off.")
+      }
   }
 })
 
+keepAlive()
 client.login(mySecret)
